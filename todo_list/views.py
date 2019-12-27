6e95c9c9e0049d6f70cfd614570251e293561c4f
stephen.mcdonagh <stephen.mcdonagh@bearingpoint.com>
2019-12-27 13:22:29 +0000

    POST request set up and tested

--- todo_list/views.py
+++ todo_list/views.py
@@ -3,16 +3,16 @@ from .models import List
 from .forms import ListForm
 from django.contrib import messages
 
-def home(requests):
+def home(request):
 	if request.method =="POST":
 		form = ListForm(request.POST or None)
 
 		if form.is_valid():
 			form.save()
 			all_items = List.objects.all
-			messsages.success(request, ('Item Successfully saved'))
-			return render(request, 'home.html', {'all_items', all_items})
+			messages.success(request, ('Item Successfully saved'))
+			return render(request, 'home.html', {'all_items': all_items})
 
 	else:
 		all_items = List.objects.all
-		return render(request, 'home.html', {'all_items', all_items})
+		return render(request, 'home.html', {'all_items': all_items})
