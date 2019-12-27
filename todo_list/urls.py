ecf3c6f53d79ccfe911eff3cace930d800ff63ae
stephen.mcdonagh <stephen.mcdonagh@bearingpoint.com>
2019-12-27 13:45:24 +0000

    Delete item from to-do-app

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -17,5 +17,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-	path('', views.home, name ="home")
+	path('', views.home, name ="home"),
+	path('delete/<list_id>', views.delete, name="delete"),
 ]
