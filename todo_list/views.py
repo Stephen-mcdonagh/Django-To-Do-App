afa49602dbf293a2dd05193bb1392055a3f28705
stephen.mcdonagh <stephen.mcdonagh@bearingpoint.com>
2019-12-27 12:36:45 +0000

    Added forms

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from .models import List
+from .forms import ListForm
+from django.contrib import messages
 
 def home(requests):
-	return render(requests, "home.html", {})
+	if request.method =="POST":
+		form = ListForm(request.POST or None)
+
+		if form.is_valid():
+			form.save()
+			all_items = List.objects.all
+			messsages.success(request, ('Item Successfully saved'))
+			return render(request, 'home.html', {'all_items', all_items})
+
+	else:
+		all_items = List.objects.all
+		return render(request, 'home.html', {'all_items', all_items})
